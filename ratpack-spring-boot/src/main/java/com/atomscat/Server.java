f35c0ab85f69a15f6615387e66279e7bb12a2e0c
HowellYang <1726474652@qq.com>
2018-11-05 00:12:38 +0800

    init spring boot 2.0.5

---
+++ ratpack-spring-boot/src/main/java/com/atomscat/Server.java
@@ -0,0 +1,39 @@
+package com.atomscat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import ratpack.func.Action;
+import ratpack.handling.Chain;
+import ratpack.service.Service;
+import ratpack.spring.config.EnableRatpack;
+
+/**
+ * Created by Howell on 28/2/17.
+ * e-mail:th15817161961@gmail.com
+ */
+@SpringBootApplication
+@EnableRatpack
+public class Server {
+    @Bean
+    public Action<Chain> home() {
+        return chain -> chain
+                .get(ctx -> ctx
+                        .render("Hello " + service().message())
+                );
+    }
+
+    @Bean
+    public Service service() {
+        return () -> "World!";
+    }
+
+    interface Service {
+        String message();
+    }
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Server.class, args);
+    }
+}
