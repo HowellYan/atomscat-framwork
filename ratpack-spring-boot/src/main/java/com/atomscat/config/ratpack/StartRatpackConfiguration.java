92aa8b6ee9066abce30b7d92ec4f00246672b011
HowellYang <1726474652@qq.com>
2018-11-05 00:34:47 +0800

    init spring boot 2.0.5

--- ratpack-spring-boot/src/main/java/com/atomscat/config/ratpack/StartRatpackConfiguration.java
+++ ratpack-spring-boot/src/main/java/com/atomscat/config/ratpack/StartRatpackConfiguration.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import ratpack.func.Function;
 import ratpack.guice.Guice;
 import ratpack.registry.Registry;
