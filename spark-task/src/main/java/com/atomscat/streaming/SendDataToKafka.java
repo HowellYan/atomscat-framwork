c937d27da12310fa20940e878dd8d69f5f729c98
HowellYang <th15817161961@gmail.com>
2018-12-18 17:14:26 +0800

    spark

--- spark-task/src/main/java/com/atomscat/streaming/SendDataToKafka.java
+++ spark-task/src/main/java/com/atomscat/streaming/SendDataToKafka.java
@@ -10,24 +10,28 @@ import java.util.*;
 public class SendDataToKafka {
 
     public static void sendData(MatrixFactorizationModel bestModel, String user, String goodsCategory) {
-        Rating[] ratingsList = bestModel.recommendProducts(Integer.valueOf(user), 2);
-        String brokers = "192.168.31.166:9092";
-        String topics = "spark-als";
-        Set<String> topicsSet = new HashSet(Arrays.asList(topics.split(",")));
-        Map<String, Object> kafkaParams = new HashMap();
-        kafkaParams.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        kafkaParams.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        kafkaParams.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-        kafkaParams.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-        kafkaParams.put("bootstrap.servers", brokers);
-        kafkaParams.put("group.id", topics);
+        try {
+            Rating[] ratingsList = bestModel.recommendProducts(Integer.valueOf(user), 2);
+            String brokers = "192.168.31.166:9092";
+            String topics = "spark-als";
+            Set<String> topicsSet = new HashSet(Arrays.asList(topics.split(",")));
+            Map<String, Object> kafkaParams = new HashMap();
+            kafkaParams.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+            kafkaParams.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+            kafkaParams.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+            kafkaParams.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+            kafkaParams.put("bootstrap.servers", brokers);
+            kafkaParams.put("group.id", topics);
 
-        KafkaProducer kafkaProducer = new KafkaProducer<String, String>(kafkaParams);
-        ProducerRecord record = new ProducerRecord<String, String>(topics, user);
-        kafkaProducer.send(record);
-        for (Rating rating : ratingsList) {
-            record = new ProducerRecord<String, String>(topics, goodsCategory + "," + String.valueOf(rating.user()) + "," + String.valueOf(rating.product()));
+            KafkaProducer kafkaProducer = new KafkaProducer<String, String>(kafkaParams);
+            ProducerRecord record = new ProducerRecord<String, String>(topics, user);
             kafkaProducer.send(record);
+            for (Rating rating : ratingsList) {
+                record = new ProducerRecord<String, String>(topics, goodsCategory + "," + String.valueOf(rating.user()) + "," + String.valueOf(rating.product()));
+                kafkaProducer.send(record);
+            }
+        } catch (Exception e){
+            System.out.println(e.getMessage().toString());
         }
     }
 }
