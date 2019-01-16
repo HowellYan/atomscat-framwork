42a9e124a2655578f4067f30e062b4a4286fab2b
HowellYang <1726474652@qq.com>
2019-01-16 22:21:09 +0800

    init spring boot 2.0.5

--- dubbo-spring-provider/dubbo-spring-provider-web/src/main/java/com/atomscat/DubboProviderDemo.java
+++ dubbo-spring-provider/dubbo-spring-provider-web/src/main/java/com/atomscat/DubboProviderDemo.java
@@ -2,8 +2,10 @@ package com.atomscat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableAutoConfiguration
+//@EnableAutoConfiguration
+@SpringBootApplication(scanBasePackages = {"com.atomscat"})
 public class DubboProviderDemo {
 
     public static void main(String[] args) {
